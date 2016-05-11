3394be84578b07940bdea49c839fe22eeaf4f113
Guriev Nicholas <guriev-ns@ya.ru>
2016-05-11 16:41:59 +0300

    include ctime

--- gcc/diagnostic-xml.c
+++ gcc/diagnostic-xml.c
@@ -22,6 +22,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "system.h"
 #include "coretypes.h"
 #include "diagnostic.h"
+#include <ctime>
 #include <string>
 
 #include "diagnostic-xml.h"
